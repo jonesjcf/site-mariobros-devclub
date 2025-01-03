039e8d4f4dc6630deec5dfce2322dc08e78bc552
jonesjcf <jones_conceicao@hotmail.com>
2025-01-03 14:22:10 -0300

    formatação do código scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
-function cliqueiNoBotao() {
+function cliqueiNoBotao(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
 }
 
-function cliqueinamascara() {
+function cliqueinamascara(){
     form.style.left = "-10%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "hidden"
